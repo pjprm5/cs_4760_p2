fb28e41e96a317136442a4fbb6a156996d9ac77e
paul passiglia <passigli@hoare7.cs.umsl.edu>
2020-10-07 01:00:22 -0500

    Made progress

--- master.c
+++ master.c
@@ -13,14 +13,29 @@
 #include <ctype.h>
 #include <getopt.h>
 #include <errno.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
 
 int main (int argc, char *argv[])
 {
+  if (argc < 2)
+  {
+    fprintf(stderr, "Error: Missing arguments.\nUsage 1: ./master -h for help.\nUsage 2: ./master -n # -s # -t # to run program. \n");
+    return (-1);
+  }
+
   
-  int option;                // Holds option values.
-  int maxChildCreated = 0;   // Option -n; Holds max # of children to be created ever.
-  int maxChildRunning = 0;   // Option -s; Holds max # of children to be running together
-  int timeTillTerm = 0;      // Option -t; Holds time limit until all processes must end.
+  int option;                 // Holds option values.
+  int maxChildCreated = -1;   // Option -n; Holds max # of children to be created ever.
+  int maxChildRunning = -1;   // Option -s; Holds max # of children to be running together
+  int timeTillTerm = -1;      // Option -t; Holds time limit until all processes must end.
+  int pr_count = 0;           // Holds number of active processes
+  
+  // Variables for allocating, deallocating shared memory
+  int shmid;
+  char sharedArray[800];
+  
+  
 
   while ((option = getopt (argc, argv, "hn:s:t:" )) != -1)
   {
@@ -35,7 +50,7 @@ int main (int argc, char *argv[])
         maxChildCreated = atoi(optarg);
         if (maxChildCreated > 20 || maxChildCreated < 1)
         {
-          fprintf(stderr, "Error; -n option must be from numbers 1-20. \n");
+          fprintf(stderr, "Error: -n option must be from numbers 1-20. \n");
         }
         //fprintf(stderr, "Option -n: You chose %d max processes to be created. \n", maxChildCreated);
         //default 4 on printout
@@ -47,7 +62,7 @@ int main (int argc, char *argv[])
         maxChildRunning = atoi(optarg);
         if (maxChildRunning > 20 || maxChildRunning < 1)
         {
-          fprintf(stderr, "Error; -s option must be from numbers 1-20. \n");
+          fprintf(stderr, "Error: -s option must be from numbers 1-20. \n");
         }
         //fprintf(stderr, "Option -s: You chose %d child processes to exist at the same time. \n", maxChildRunning);
         //default 2 on printout
@@ -83,17 +98,66 @@ int main (int argc, char *argv[])
         
         return -1;
       default:
-         fprintf(stderr, "Default error. \n");
-         return -1;
+        fprintf(stderr, "Default error. \n");
+        return -1;
     }
   }
+  // Default value logic, not sure about it yet
+  if (maxChildCreated == -1 || maxChildRunning == -1 || timeTillTerm == -1)
+  {
+    if (maxChildCreated == -1)
+    {
+      maxChildCreated = 4;
+    }
+    if (maxChildRunning == -1)
+    {
+      maxChildRunning = 2;
+    }
+    if (timeTillTerm == -1)
+    {
+      timeTillTerm = 100;
+    }
+  }
+  
+
+  // Allocate shared memory
+
+  // Key generator for shared memory
+  key_t key = ftok("./master", 'j');
+  printf("%d is the key.\n", key);
+  // Set shared memory ID
+  if ((shmid = shmget(key, 1024, 0600 | IPC_CREAT)) == -1)
+  {
+    perror("Master: Error: shmget");
+    exit(1);
+  }
+
+  // Attach
+  sharedArray = shmat(shmid, NULL, 0);
+  if  
 
   
-  fprintf(stderr, "Option -n: You chose %d max processes to be create. \n", maxChildCreated);
+  
+
+  // Open file stream to gain access to infile which contains the palindromes to be tested
+ /* FILE *fptr;
+  if ((fptr = fopen("infile", "r")) == NULL)
+  {
+    fprintf(stderr, "Error! File will not open. \n");
+    return -1;
+  }*/
+
+  // Need loop here to scan infile and put it into shared memory like sarr[800]
+
+
+  
+  fprintf(stderr, "Option -n: You chose %d max processes to be created. \n", maxChildCreated);
   fprintf(stderr, "Option -s: You chose %d child processes to exist at the same time. \n", maxChildRunning);
   fprintf(stderr, "Option -t: You chose %d seconds until termination of program. \n", timeTillTerm);
   printf("Program ended. \n");
-
+  
+  //Close file 
+  fclose(fptr);
 
   return 0;
 }
--- palin.c
+++ palin.c
@@ -6,9 +6,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// isPalindrome function below sourced from geeksforgeeks.org/c-program-check-given-string-palindrome/
+void isPalindrome(char str[])
+{
+  // Start from leftmost and rightmost corners of str
+  int l = 0;
+  int h = strlen(str) - 1;
+  // Keep comparing characters while they are same
+  while (h > l)
+  {
+    if (str[l++] != str[h--])
+    {
+      printf("%s is not a palindrome.\n", str);
+      return;
+    }
+  }
+  printf("%s is a palindrome!.\n", str);
+}
+
 
 int main(int argc, char *argv[])
 {
+ char str1[80] = "hello";
+ char str2[80] = "racecar";
+ 
+ isPalindrome(str1);
+ isPalindrome(str2);
+
+ //function check if string is palindrome
+ //execute code to enter critical section which is when your writing out to the palin.out and nopalin.out
+ //sleep for a random amount of time (between 0 and 2 seconds)
+ //execute code to exit from critical section
+  
   printf("Testing...\n");
   return 0;
 }
