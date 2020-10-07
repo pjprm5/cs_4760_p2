c60c0fe014a0fdcd3206f291d2577fccc7fd3772
paul passiglia <passigli@hoare7.cs.umsl.edu>
2020-10-07 18:30:35 -0500

    Finished sharedArray

--- master.c
+++ master.c
@@ -29,14 +29,31 @@ int main (int argc, char *argv[])
   int maxChildCreated = -1;   // Option -n; Holds max # of children to be created ever.
   int maxChildRunning = -1;   // Option -s; Holds max # of children to be running together
   int timeTillTerm = -1;      // Option -t; Holds time limit until all processes must end.
-  int pr_count = 0;           // Holds number of active processes
   
   // Variables for allocating, deallocating shared memory
-  int shmid;
-  char sharedArray[800];
   
-  
+  int shmid;                  // Contains segment ID
+  char *sharedArray;          // Shared memory array contains infile contents
+  char tempArray[800];        // Hoping to store infile contents in here
+  char *buffer = NULL;        // Buffer for getline  
+  size_t bufferSize = 80;     // Buffer size for getline
+  int line_count = 0;         // Keep track of lines in infile
+  int i;                      // Loop counter
+  int buffArray[100];         // Keep track of buffer sizes for each palidrome during getline process
 
+  // Variables for fork() exec()
+
+  pid_t childpid = 0;
+  int pr_count = 0;                // Holds number of active processes
+  int pr_limit = maxChildCreated;  // Holds max allowed processes to be created
+  
+  // Initialize tempArray
+  for (i = 0; i < 800; i++)
+  {
+    tempArray[i] = ' ';
+  }
+  
+  // Getopt Menu Loop
   while ((option = getopt (argc, argv, "hn:s:t:" )) != -1)
   {
     switch (option)
@@ -118,6 +135,8 @@ int main (int argc, char *argv[])
       timeTillTerm = 100;
     }
   }
+
+
   
 
   // Allocate shared memory
@@ -132,22 +151,134 @@ int main (int argc, char *argv[])
     exit(1);
   }
 
-  // Attach
-  sharedArray = shmat(shmid, NULL, 0);
-  if  
+  // Attach to the shared memory
+  sharedArray = (char *)shmat(shmid, (void *)0, 0);
+  if (sharedArray == (char *)(-1))
+  {
+    perror("Master: Error: shmat");
+    exit(1);
+  }
+  
+  // Copy a string into shared memory for testing
+  //strncpy(sharedArray, test, 7);
+
+  //printf("Shared memory contains: %s \n", sharedArray);
+
+  // Detach from segment.
+  /*if (shmdt (sharedArray) == -1)
+  {
+    perror("Master: Error: shmdt");
+    exit(1);
+  }*/
+
+
+  
+  // Deallocate shared memory segment
+
+ /* if (shmctl(shmid, IPC_RMID, NULL) == -1)
+  {
+    perror("Master: Error: shmctl");
+  }*/
+
 
   
   
 
   // Open file stream to gain access to infile which contains the palindromes to be tested
- /* FILE *fptr;
+  FILE *fptr;
   if ((fptr = fopen("infile", "r")) == NULL)
   {
     fprintf(stderr, "Error! File will not open. \n");
     return -1;
+  }
+  
+
+  // Trying to copy infile into tempArray
+  
+  while (getline(&buffer, &bufferSize, fptr) != -1)
+  {
+    printf("Line 1: %s\n", buffer);
+    buffArray[line_count] = strlen(buffer);
+
+    for (i = 0; i < strlen(buffer); i++)
+    {
+       tempArray[line_count * 80 + i] = buffer[i];
+       printf("%c", tempArray[line_count * 80 + i]);
+    }
+
+    //tempArray[line_count * 80 + (i+1)] = '\0';
+    //printf("Null should print nothing here:%c", tempArray[line_count * 80 + (i+1)]);
+    printf("\n");
+
+    line_count++; 
+  }
+  
+  // Now copy tempArray into sharedArray...
+  strncpy(sharedArray, tempArray, 800);
+  
+  
+  /*// Display whats in sharedArray...
+  for (i = 0; i < 800; i++)
+  {
+    printf("%c", sharedArray[i]); 
+  }*/
+  
+  /*// buffArray display
+  for (i = 0; i < line_count; i++)
+  {
+    printf("Buff array:%d", buffArray[i]);
   }*/
 
-  // Need loop here to scan infile and put it into shared memory like sarr[800]
+  
+  // Now add nulls into sharedArray...
+  
+  for (i = 0; i < line_count; i++)
+  {
+    sharedArray[i * 80 + buffArray[i]] = '\0';
+  }
+
+ 
+  /* //buffArray display again
+  for (i = 0; i < 800; i++)
+  {
+    printf("%c", sharedArray[i]);
+  }
+ */
+
+  //printf("Should print d here --> %c\n", sharedArray[1*80 + 3]);
+
+  //***************** BEGIN FORKING/EXEC CHILDREN *****************//
+  if ((childpid = fork()) <= 0)
+  {
+    //break;
+    printf("Child released!\n");
+  }
+  
+  // Check to see if fork failed
+  if (childpid == -1)
+  {
+    perror("Master: Error: Fork failed: ");
+    return -1;
+  }
+  
+  // Exec() child
+  execl("palin", "palin", NULL);
+  
+
+  // Detach from segment
+  if (shmdt (sharedArray) == -1)
+  {
+    perror("Master: Error: shmdt");
+    exit(1);
+  }
+  
+  // Deallocate shared memory segment
+  if (shmctl(shmid, IPC_RMID, NULL) == -1)
+  {
+    perror("Master: Error: shmctl");
+    exit(1);
+  }
+
 
 
   
