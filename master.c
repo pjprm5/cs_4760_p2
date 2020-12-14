3e7bdb6bb087e96bd6257f541ae224e74df8fc5d
paul passiglia <passigli@hoare7.cs.umsl.edu>
2020-12-13 21:48:38 -0600

    Project 7 redo commit

--- master.c
+++ master.c
@@ -4,7 +4,6 @@
 //Date: 10/5/2020
 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,43 +14,52 @@
 #include <errno.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
+#include <time.h>
+#include "palinArray.h"
+#include <semaphore.h>
+
+
+// Prototypes
+void raiseAlarm();
+
+// Global vars
+PalArray *sharedArray;
+int shmid;
 
 int main (int argc, char *argv[])
 {
+  /*
   if (argc < 2)
   {
     fprintf(stderr, "Error: Missing arguments.\nUsage 1: ./master -h for help.\nUsage 2: ./master -n # -s # -t # to run program. \n");
     return (-1);
-  }
+  }*/
+
+  signal(SIGALRM, raiseAlarm);  // Start Alarm.
 
   
-  int option;                 // Holds option values.
-  int maxChildCreated = -1;   // Option -n; Holds max # of children to be created ever.
-  int maxChildRunning = -1;   // Option -s; Holds max # of children to be running together
-  int timeTillTerm = -1;      // Option -t; Holds time limit until all processes must end.
+  int option;                // Holds option values.
+  int maxChildCreated = 4;   // Option -n; Holds max # of children to be created ever.
+  int maxChildRunning = 2;   // Option -s; Holds max # of children to be running together
+  int timeTillTerm = 10;    // Option -t; Holds time limit until all processes must end.
   
-  // Variables for allocating, deallocating shared memory
+  struct timespec ts1, ts2;  // For nanosleep iteration smoother
+  ts1.tv_sec = 0;
+  ts1.tv_nsec = 500L;
   
-  int shmid;                  // Contains segment ID
-  char *sharedArray;          // Shared memory array contains infile contents
-  char tempArray[800];        // Hoping to store infile contents in here
   char *buffer = NULL;        // Buffer for getline  
   size_t bufferSize = 80;     // Buffer size for getline
   int line_count = 0;         // Keep track of lines in infile
   int i;                      // Loop counter
   int buffArray[100];         // Keep track of buffer sizes for each palidrome during getline process
-
-  // Variables for fork() exec()
-
-  pid_t childpid = 0;
-  int pr_count = 0;                // Holds number of active processes
-  int pr_limit = maxChildCreated;  // Holds max allowed processes to be created
   
-  // Initialize tempArray
-  for (i = 0; i < 800; i++)
-  {
-    tempArray[i] = ' ';
-  }
+  int pr_count = 0;           // Holds number of processes created
+  int pr_active = 0;          // Holds active number of processes
+
+  // Flags for getopt
+  int optN = 0;
+  int optS = 0;
+  int optT = 0;
   
   // Getopt Menu Loop
   while ((option = getopt (argc, argv, "hn:s:t:" )) != -1)
@@ -60,129 +68,139 @@ int main (int argc, char *argv[])
     {
 
       case 'h' :
-        fprintf(stderr, "Explanation here.\n");
+        fprintf(stderr, "Usage: ./master -n # -s # -t #\n");
+        fprintf(stderr, "Master will fork() and exec() children to execute ./palin which checks\n");
+        fprintf(stderr, "each string inside infile to determine if it is a palindrome or not.\n");
+        fprintf(stderr, "-n decides max child processes to be created in total. Defaults to 4 if not specified.\n");
+        fprintf(stderr, "-s decides max child processes to be running simultaneously. Defaults to 2 if not specified.\n");
+        fprintf(stderr, "-t is the amount of time until the program terminates all children and itself. Defaults to 40 if not specified.\n");
+
         return -1;
 
       case 'n' :
+        if (atoi(optarg) == 0)
+        {
+          perror("MASTER: Error: The -n option must be an integer from 1-20: \n");
+          return -1;
+        }
         maxChildCreated = atoi(optarg);
         if (maxChildCreated > 20 || maxChildCreated < 1)
         {
-          fprintf(stderr, "Error: -n option must be from numbers 1-20. \n");
+          fprintf(stderr, "MASTER: Error: -n option must be an integer from 1-20. \n");
+          return -1;
         }
-        //fprintf(stderr, "Option -n: You chose %d max processes to be created. \n", maxChildCreated);
-        //default 4 on printout
-        else
-        {
-          break;
-        }
+       
+        optN = 1; // Set flag.
+        break;
+        
       case 's' :
+        if (atoi(optarg) == 0)
+        {
+          perror("MASTER: Error: the -s option must be an integer from 1-20. \n");
+          return -1;
+        }
         maxChildRunning = atoi(optarg);
         if (maxChildRunning > 20 || maxChildRunning < 1)
         {
-          fprintf(stderr, "Error: -s option must be from numbers 1-20. \n");
+          fprintf(stderr, "MASTER: Error: -s option must be an integer from 1-20. \n");
+          return -1;
         }
-        //fprintf(stderr, "Option -s: You chose %d child processes to exist at the same time. \n", maxChildRunning);
-        //default 2 on printout
-        else
-        {
-          break;
-        }
+        
+        optS = 1; // Set flag.
+        break;
+        
       case 't' :
+        if (atoi(optarg) == 0)
+        {
+          perror("MASTER: Error: The -t option must be an integer from 1-100. \n");
+          return -1;
+        }
         timeTillTerm = atoi(optarg);
-        if (timeTillTerm > 100 || timeTillTerm < 1)
+        if (timeTillTerm > 100 || timeTillTerm < 40)
         {
-          fprintf(stderr, "Error: -t option must be from numbers 1-100. \n");
+          fprintf(stderr, "MASTER: Error: -t option must be an integer from 1-100. \n");
+          return -1;
         }
-        //fprintf(stderr, "Option -t: You chose %d seconds until termination of program. \n", timeTillTerm);
-        //default 100 on printout
-        else
-        {
-          break;
-        }
-      case '?' :
+        
+        optT = 1; // Set flag.
+        break;
+        
+      case '?' : // Deal with unknowns.
         if (optopt == 'n')
         {
-          fprintf(stderr, "Option -%c requires an argument. \n", optopt);
+          fprintf(stderr, "MASTER: Error: Option -c requires an argument. \n");
+          return -1;
         }
         if (optopt == 's')
         {
-          fprintf(stderr, "Option -%c requires an argument. \n", optopt);
+          fprintf(stderr, "MASTER: Error: Option -s requires an argument. \n");
+          return -1;
         }
         if (optopt == 't')
         {
-          fprintf(stderr, "Option -%c requires an argument. \n", optopt);
+          fprintf(stderr, "MASTER: Error: Option -t requires an argument. \n");
+          return -1;
         }
-        
-        return -1;
+        else if (isprint (optopt))
+        {
+          fprintf(stderr, "MASTER: Error: Unknown option detected. \n");
+          return -1;
+        }
+        else
+        {
+          fprintf(stderr, "Error: Unknown error from options detected, \n");
+          return -1;
+        }
       default:
         fprintf(stderr, "Default error. \n");
         return -1;
     }
   }
-  // Default value logic, not sure about it yet
-  if (maxChildCreated == -1 || maxChildRunning == -1 || timeTillTerm == -1)
+
+  // Set defaults.
+  if (optN == 0)
   {
-    if (maxChildCreated == -1)
-    {
-      maxChildCreated = 4;
-    }
-    if (maxChildRunning == -1)
-    {
-      maxChildRunning = 2;
-    }
-    if (timeTillTerm == -1)
-    {
-      timeTillTerm = 100;
-    }
+    maxChildCreated = 4;
   }
 
+  if (optS == 0)
+  {
+    maxChildRunning = 2;
+  }
 
-  
+  if (optT == 0)
+  {
+    timeTillTerm = 40;
+  }
 
-  // Allocate shared memory
+  fprintf(stderr, "\nOption -n: You chose %d max processes to be created. \n", maxChildCreated);
+  fprintf(stderr, "Option -s: You chose %d child processes to exist at the same time. \n", maxChildRunning);
+  fprintf(stderr, "Option -t: You chose %d seconds until termination of program. \n", timeTillTerm);
+
+  // Allocate shared memory -------------------------------------------------
 
   // Key generator for shared memory
   key_t key = ftok("./master", 'j');
-  printf("%d is the key.\n", key);
-  // Set shared memory ID
-  if ((shmid = shmget(key, 1024, 0600 | IPC_CREAT)) == -1)
+  if (key == -1)
   {
-    perror("Master: Error: shmget");
-    exit(1);
+    perror("MASTER: Error: ftok failure.\n");
+    exit(-1);
+  }
+
+  // Set shared memory ID
+  if ((shmid = shmget(key, sizeof(PalArray), 0600 | IPC_CREAT)) == -1)
+  {
+    perror("MASTER: Error: shmget failure.\n");
+    exit(-1);
   }
 
   // Attach to the shared memory
-  sharedArray = (char *)shmat(shmid, (void *)0, 0);
-  if (sharedArray == (char *)(-1))
+  sharedArray = (PalArray *)shmat(shmid, (void *)0, 0);
+  if (sharedArray == (void *)(-1))
   {
-    perror("Master: Error: shmat");
-    exit(1);
+    perror("MASTER: Error: shmat failure.\n");
+    exit(-1);
   }
-  
-  // Copy a string into shared memory for testing
-  //strncpy(sharedArray, test, 7);
-
-  //printf("Shared memory contains: %s \n", sharedArray);
-
-  // Detach from segment.
-  /*if (shmdt (sharedArray) == -1)
-  {
-    perror("Master: Error: shmdt");
-    exit(1);
-  }*/
-
-
-  
-  // Deallocate shared memory segment
-
- /* if (shmctl(shmid, IPC_RMID, NULL) == -1)
-  {
-    perror("Master: Error: shmctl");
-  }*/
-
-
-  
-  
 
   // Open file stream to gain access to infile which contains the palindromes to be tested
   FILE *fptr;
@@ -191,104 +209,139 @@ int main (int argc, char *argv[])
     fprintf(stderr, "Error! File will not open. \n");
     return -1;
   }
-  
 
-  // Trying to copy infile into tempArray
-  
+  alarm(timeTillTerm); // Set alarm
+
+  // Trying to copy infile into sharedArray -----------------------------------
+  sem_init(&(sharedArray->mutex), 1, 1);
+  sem_wait(&(sharedArray->mutex));
   while (getline(&buffer, &bufferSize, fptr) != -1)
   {
-    printf("Line 1: %s\n", buffer);
+    //printf("Line %d: %s\n", line_count, buffer);
     buffArray[line_count] = strlen(buffer);
 
     for (i = 0; i < strlen(buffer); i++)
     {
-       tempArray[line_count * 80 + i] = buffer[i];
-       printf("%c", tempArray[line_count * 80 + i]);
+       sharedArray->palinArray[line_count * 80 + i] = buffer[i];
+       //printf("%c", sharedArray->palinArray[line_count * 80 + i]);
     }
-
-    //tempArray[line_count * 80 + (i+1)] = '\0';
-    //printf("Null should print nothing here:%c", tempArray[line_count * 80 + (i+1)]);
-    printf("\n");
+    //printf("\n");
 
     line_count++; 
   }
-  
-  // Now copy tempArray into sharedArray...
-  strncpy(sharedArray, tempArray, 800);
-  
-  
-  /*// Display whats in sharedArray...
-  for (i = 0; i < 800; i++)
-  {
-    printf("%c", sharedArray[i]); 
-  }*/
-  
-  /*// buffArray display
-  for (i = 0; i < line_count; i++)
-  {
-    printf("Buff array:%d", buffArray[i]);
-  }*/
-
-  
-  // Now add nulls into sharedArray...
-  
-  for (i = 0; i < line_count; i++)
-  {
-    sharedArray[i * 80 + buffArray[i]] = '\0';
-  }
-
- 
-  /* //buffArray display again
-  for (i = 0; i < 800; i++)
-  {
-    printf("%c", sharedArray[i]);
-  }
- */
-
-  //printf("Should print d here --> %c\n", sharedArray[1*80 + 3]);
-
-  //***************** BEGIN FORKING/EXEC CHILDREN *****************//
-  if ((childpid = fork()) <= 0)
-  {
-    //break;
-    printf("Child released!\n");
-  }
-  
-  // Check to see if fork failed
-  if (childpid == -1)
-  {
-    perror("Master: Error: Fork failed: ");
-    return -1;
-  }
-  
-  // Exec() child
-  execl("palin", "palin", NULL);
-  
-
-  // Detach from segment
-  if (shmdt (sharedArray) == -1)
-  {
-    perror("Master: Error: shmdt");
-    exit(1);
-  }
-  
-  // Deallocate shared memory segment
-  if (shmctl(shmid, IPC_RMID, NULL) == -1)
-  {
-    perror("Master: Error: shmctl");
-    exit(1);
-  }
-
-
-
-  
-  fprintf(stderr, "Option -n: You chose %d max processes to be created. \n", maxChildCreated);
-  fprintf(stderr, "Option -s: You chose %d child processes to exist at the same time. \n", maxChildRunning);
-  fprintf(stderr, "Option -t: You chose %d seconds until termination of program. \n", timeTillTerm);
-  printf("Program ended. \n");
-  
-  //Close file 
+  // Close file
   fclose(fptr);
 
+  // Write a begin statement into both palin.out and nopalin.out
+  fptr = fopen("palin.out", "w");
+  fprintf(fptr, "--------------- BEGIN ----------------\n");
+  fclose(fptr);
+
+  fptr = fopen("nopalin.out", "w");
+  fprintf(fptr, "--------------- BEGIN ----------------\n");
+  fclose(fptr);
+  
+  // Now add nulls into sharedArray
+  for (i = 0; i < line_count; i++)
+  {
+    sharedArray->palinArray[i * 80 + buffArray[i]] = '\0';
+  } 
+  sem_post(&(sharedArray->mutex));
+
+  // Print array
+  /*for (i = 0; i < 1600; i++)
+  { 
+    
+    printf("%c", sharedArray->palinArray[i]);
+  }
+  printf("\n");*/
+
+  //***************** BEGIN FORKING/EXEC CHILDREN *****************//--------------
+  pid_t childPid;
+  int childControl = maxChildCreated; 
+  int j;
+  int k;
+  
+  // Get pr_count as a string to shoot off index of palindrome 
+  char indexToCheck[2];
+  snprintf (indexToCheck, sizeof(indexToCheck), "%d", pr_count);
+  //printf("TEST MASTER ---->: pr_count: %s \n", indexToCheck);
+
+  for (i = 0; i < childControl; i++)
+  {
+    if (pr_active <= maxChildRunning)
+    {
+      wait(NULL); // wait for child to die
+      if (pr_active != 0)
+      {
+        pr_active--; // decrement active children
+      }
+      if (pr_count <= maxChildCreated)
+      {
+        childPid = fork(); // fork child
+      }
+
+      if (childPid == 0 && pr_count <= maxChildCreated)
+      {   
+        char *args[] = {"./palin", indexToCheck, NULL};
+        execvp(args[0], args);
+      }
+
+      else if (childPid < 0)
+      {
+        childControl++; // fork fails, increment loop by one to try again
+        printf("Child fork failed.\n");
+        while(nanosleep(&ts1, &ts2)); // retry loop
+      }
+
+      else if (childPid > 0 && pr_count <= maxChildCreated)
+      {
+        pr_active++;
+        pr_count++;
+        snprintf (indexToCheck, 4, "%d", pr_count);
+        //printf("MASTER TEST 2: %s \n", indexToCheck);
+        printf("\nMASTER: Child#: %d with pid: %d is born -------> \n", pr_count, childPid);
+      }
+      
+      // Start deallocation once process count reaches max child creation
+      if (pr_count == maxChildCreated)
+      {
+        //printf("pr_count: %d \n", pr_count);
+
+        // Wait for any children left to die
+        for (j = 0; j < maxChildCreated; j++)
+        {
+          // printf("Test 1\n");
+          wait(NULL);
+        }
+
+        sleep(1); // sleep for 1 second to make sure before deallocation
+
+        // Detach from segment
+        if (shmdt (sharedArray) == -1)
+        {
+          perror("MASTER: Error: shmdt failure.\n");
+          exit(-1);
+        }
+  
+        // Deallocate shared memory segment
+        if (shmctl(shmid, IPC_RMID, NULL) == -1)
+        {
+          perror("Master: Error: shmctl failure.\n");
+          exit(-1);
+        }
+      }
+    }
+  }
+  printf("\nMASTER: PROGRAM ENDED. \n\n");
+
   return 0;
+}
+
+void raiseAlarm() // Kill everything within timelimit
+{
+  printf("\n Time limit hit, terminating all processes. \n");
+  shmdt(sharedArray);
+  shmctl(shmid, IPC_RMID, NULL);
+  kill(0, SIGKILL);
 }
--- palin.c
+++ palin.c
@@ -5,41 +5,135 @@
 
 
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include "palinArray.h"
+#include <semaphore.h>
+
+// Global vars
+int shmid;
+PalArray *sharedArray;
 
 // isPalindrome function below sourced from geeksforgeeks.org/c-program-check-given-string-palindrome/
-void isPalindrome(char str[])
+int isPalindrome(char str[])
 {
   // Start from leftmost and rightmost corners of str
   int l = 0;
   int h = strlen(str) - 1;
   // Keep comparing characters while they are same
-  while (h > l)
+  while (h > l && strlen(str) != 1)
   {
     if (str[l++] != str[h--])
     {
-      printf("%s is not a palindrome.\n", str);
-      return;
+      printf("%s is NOT a palindrome.\n", str);
+      return 2;
     }
   }
-  printf("%s is a palindrome!.\n", str);
+  printf("%s is a palindrome!\n", str);
+  return 1;
 }
+
+/*int randomTime()
+{
+  int randomNum = ((rand() % (2 - 0 + 1)));
+  return randomNum;
+}*/
+
 
 
 int main(int argc, char *argv[])
-{
- char str1[80] = "hello";
- char str2[80] = "racecar";
- 
- isPalindrome(str1);
- isPalindrome(str2);
-
- //function check if string is palindrome
- //execute code to enter critical section which is when your writing out to the palin.out and nopalin.out
- //sleep for a random amount of time (between 0 and 2 seconds)
- //execute code to exit from critical section
+{ 
+  //printf("PALIN: Begin program.\n");
   
-  printf("Testing...\n");
+  char palindrome[80] = { 0 };        // Stores palindrome to check.
+  int palinIndex = atoi(argv[1]);     // Contains index to check in shared memory from master.
+  //srand(time(0) ^ getpid());          // Seed random time.
+  FILE *fptr; 
+
+  // Allocate shared memory
+  
+  key_t key = ftok("./master", 'j');
+  if (key == -1)
+  {
+    perror("PALIN: Error: ftok failure. \n");
+    exit(-1);
+  }
+
+  if ((shmid = shmget(key, sizeof(PalArray), 0600 | IPC_CREAT)) == -1)
+  {
+    perror("PALIN: Error: shmget.\n");
+    exit(-1);
+  }
+
+  sharedArray = (PalArray *)shmat(shmid, (void *)0, 0);
+  if (sharedArray == (void *)(-1))
+  {
+    perror("PALIN: Error: shmat failure.\n");
+    exit(-1);
+  }
+  
+  //int randomSleep = randomTime(); // figure out random time to sleep from 0-2 seconds
+  //printf("RANDOM SLEEP: %d \n", randomSleep);
+  // Critical Section ------------------------------------------------------------------------ 
+  sem_wait(&(sharedArray->mutex));
+  // Announce entered cs
+  fprintf(stderr, "PALIN: INDEX#: %d CHILD#: %d ENTERED CRITICAL SECTION.\n", palinIndex, getpid());
+
+  // Grab palindrome from specified index
+  //printf("Test index palindrome #%d: ", palinIndex);
+  int i;
+  int j;
+  printf("PALIN: INFILE PALINDROME CONTAINS: ");
+  for (i = 0; i < 80; i++)
+  {
+    printf("%c", sharedArray->palinArray[palinIndex * 80 + i]);
+    // Remove everything fom array besides alphabet or numbers
+    if (isalpha(sharedArray->palinArray[palinIndex * 80 + i]) || isdigit(sharedArray->palinArray[palinIndex * 80 + i]))
+    {
+      
+      palindrome[j] = sharedArray->palinArray[palinIndex * 80 + i];
+      //printf("%c", palindrome[j]);
+      j++;
+    }
+  }
+  printf("PALIN: STRING STORED TO CHECK PALINDROME STATUS: %s \n", palindrome);
+
+  // Sleep, then check palindrome status and write to palin.out or nopalin.out 
+  //printf("PALIN: SLEEPING: %d secs.\n", randomSleep);
+  sleep(1);
+  int palCheck = isPalindrome(palindrome); 
+  if (palCheck == 1)
+  {
+    //printf("%s is a palindrome. \n", palindrome);
+    fptr = fopen("palin.out", "a");
+    fprintf(fptr, "PID: %d Index: %d String: %s \n", getpid(), palinIndex, palindrome);
+    fclose(fptr);
+  }
+  if (palCheck == 2)
+  {
+    //printf("%s is NOT a palindrome. \n", palindrome);
+    fptr = fopen("nopalin.out", "a"); 
+    fprintf(fptr, "PID: %d Index: %d String: %s \n", getpid(), palinIndex, palindrome);
+    fclose(fptr);
+  }
+  
+  fprintf(stderr, "PALIN: INDEX#: %d CHILD#: %d EXITING CRITICAL SECTION.\n", palinIndex, getpid());
+
+  sem_post(&(sharedArray->mutex));
+ 
+  // Detach shared memory.
+  if (shmdt (sharedArray) == -1)
+  {
+    perror("PALIN: Error: shmdt");
+    exit(-1);
+  }
+
   return 0;
 }
